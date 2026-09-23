e985676cf7b21243d24e17fb1a928a8f5160c387
leidianeg-cmd <leidianeg185@gmail.com>
2026-09-23 15:27:14 -0300

    Change user role in mockData.ts
    Updated user role from 'Gerente' to 'Stock' in mock data.

--- src/mockData.ts
+++ src/mockData.ts
@@ -16,8 +16,8 @@ export const PREDEFINED_USERS: AuthAccount[] = [
     username: 'admin',
     password: 'admin123',
     name: 'Carlos Souza',
-    role: 'Gerente',
-    avatar: 'https://images.unsplash.com/photo-1534528741775-53994a69daeb?w=150&auto=format&fit=crop&q=80',
+    role: 'Stock',
+    
   },
   {
     email: 'almoxarifado@empresa.com.br',
